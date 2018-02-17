ef44254c22201f9bd4e3d2ce9358fe0efec94072
Enric Climent <enric612@gmail.com>
2018-02-17 16:36:54 +0100

    Models added
    Added models and helper methods

--- getstarted_django/settings.py
+++ getstarted_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'getstarted_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'getstarted_django',
     }
 }
 
