00aef364249a6e6375a383c844f00c1be41c73b4
Enric Climent <enric612@gmail.com>
2018-02-19 11:36:48 +0100

    fix and style css

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 # Create your views here.
