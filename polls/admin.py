01f6516b50cebeec804e5e3321a70c5566d518f6
Enric Climent <enric612@gmail.com>
2018-02-19 11:43:01 +0100

    add custom admin form for Question (simple form)

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Question
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
 # Register your models here.
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
