9dc2c4cb40980e7833bdcacaab047023011eb81a
Enric Climent <enric612@gmail.com>
2018-02-19 11:43:25 +0100

    Added Fieldset to Question Admin form

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
 
 # Register your models here.
 
